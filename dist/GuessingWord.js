9bfe5b6766c8e3b084fc262b1334bcc8fbcf6d74
Hung-lin Yeh <timmyyeh@cs.washington.edu>
2017-10-11 02:04:35 -0700

    Reupload

---
+++ dist/GuessingWord.js
@@ -0,0 +1,57 @@
+'use strict';
+
+$(function () {
+    var word;
+    var newWord;
+    $('#btn_1').click(function () {
+
+        $('h4').text("Here's the  word");
+        $('#btn_1').hide();
+        $('#game').show();
+        $('#word').addClass('word');
+        getNewWord();
+        $('#word').append('<h1>' + newWord + '</h1>');
+
+        $('.button').click(function (e) {
+            e.preventDefault();
+            var input = $('#guess').val();
+            if (input.toUpperCase() == word.toUpperCase()) {
+                $('#guess').val('');
+
+                $('h1').text("Correct!");
+                setTimeout(function () {
+                    getNewWord();
+                }, 1000);
+            } else {
+                $('h1').text("Wrong!");
+                $('#guess').val('');
+            }
+        });
+    });
+    function randomWord(word) {
+        var a = word.split("");
+        for (var i = 0; i < word.length; i++) {
+            var index = Math.floor(Math.random() * word.length);
+            var temp = a[i];
+            a[i] = a[index];
+            a[index] = temp;
+        }
+        return a.join('');
+    }
+    function getNewWord() {
+        $.ajax({
+            url: 'words.json',
+            type: 'GET',
+            async: false,
+            success: function success(data) {
+
+                var index = Math.floor(Math.random() * 4);
+                word = data.words[index];
+                console.log(word);
+                newWord = randomWord(word);
+            }
+        });
+        $('h1').text('' + newWord);
+    }
+});
+//# sourceMappingURL=GuessingWord.js.map
---
+++ dist/src/test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+nums = [2, 7, 3, 4, 5];
+var target = 9;
+var twoNum = function twoNum(num, targer) {
+    num.map(function (num, index) {});
+};
+//# sourceMappingURL=test.js.map
